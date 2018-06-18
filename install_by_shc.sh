e24cafcadbccfa9503e4a878240a349f633aa4e9
odddd0 <odddd0@github.com>
2018-06-18 10:00:54 +0800

    [SHC] install script add 30 days expiration.

--- install_by_shc.sh
+++ install_by_shc.sh
@@ -3,8 +3,17 @@
 cd `dirname $0`
 workpath=`pwd`/src
 
+cd ~
+homepath=`pwd`
+binpath=$homepath/bin
+
 mkdir $workpath/../bin 2>/dev/null
 mkdir $workpath/../build 2>/dev/null
+
+curTime=`date +%s`
+# one month later 
+expirationTime=`expr $curTime + 2592000`
+expirationTimeStr=`date -r $expirationTime +%d/%m/%Y`
 
 for cur in $workpath/*
 do
@@ -13,6 +22,9 @@ do
 	if [ "$fileGeneral" == "23212F62696E2F62617368" ] ; then
 		fileName=`basename $cur`
 		cp $cur $workpath/../build/$fileName
-		shc -f $workpath/../build/$fileName -o $workpath/../bin/$fileName
+		shc -e $expirationTimeStr -f $workpath/../build/$fileName -o $workpath/../bin/$fileName
+		if [ "$1" == "-i" ] ; then
+			cp $workpath/../bin/$fileName $binpath
+		fi
 	fi
 done
